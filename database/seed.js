58fbd75e40f9756e65df36c3723dee0baf1182c6
Simon Neutert <simonneutert@users.noreply.github.com>
2025-08-02 15:25:01 +0200

    Refactor code style for consistency and clarity across multiple files

--- database/seed.js
+++ database/seed.js
@@ -1,39 +1,38 @@
-const db = require('./config');
+const db = require("./config");
 
-module.exports = function() {
-  const seedDatabase = function() {
-    var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+module.exports = function () {
+  const seedDatabase = function () {
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
     for (var i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + i)
+      stmt.run("Ipsum " + i);
     }
-    stmt.finalize()
-  }
+    stmt.finalize();
+  };
 
   try {
-    db.serialize(function() {
-      db.run('CREATE TABLE lorem (info TEXT)', function(e) {
+    db.serialize(function () {
+      db.run("CREATE TABLE lorem (info TEXT)", function (e) {
         if (e !== undefined) {
-          db.all('SELECT COUNT(*) FROM lorem', function(err, rows) {
-            const rowsCount = rows[0]['COUNT(*)']
+          db.all("SELECT COUNT(*) FROM lorem", function (err, rows) {
+            const rowsCount = rows[0]["COUNT(*)"];
             if (rowsCount <= 0) {
-              console.log("seeding data ...")
-              seedDatabase()
+              console.log("seeding data ...");
+              seedDatabase();
             }
-          })
+          });
         }
-      })
-    })
+      });
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   } finally {
-    db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-      console.log('Current content in lorem table:')
+    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+      console.log("Current content in lorem table:");
       if (err) {
-        console.log(err)
+        console.log(err);
       } else {
-        console.log(row.id + ': ' + row.info)
+        console.log(row.id + ": " + row.info);
       }
-    })
+    });
   }
-
-}
+};
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Requirements
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // App
 var app = express();
-const port = 3000
+const port = 3000;
 
 // SEEDING
-const seedDatabase = require('./database/seed');
-seedDatabase()
+const seedDatabase = require("./database/seed");
+seedDatabase();
 
 // Logging
-const timestamp = require('./config/router/timestamp');
-app.use(timestamp)
+const timestamp = require("./config/router/timestamp");
+app.use(timestamp);
 
 // ROUTES
-var rootRoutes = require('./routes/root');
-app.use('/', rootRoutes);
-var welcomeRoutes = require('./routes/welcome');
-app.use('/welcome', welcomeRoutes);
+var rootRoutes = require("./routes/root");
+app.use("/", rootRoutes);
+var welcomeRoutes = require("./routes/welcome");
+app.use("/welcome", welcomeRoutes);
 
 // Start Server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
